55bbfe31fc71329aaa9453ae0a7828f0a25b2d5a
Konrad Depowski <konrad6depowski@wp.pl>
2021-12-13 17:47:19 +0100

    main

--- main.js
+++ main.js
@@ -6,3 +6,25 @@ const santaMove = () => {
 }
 santaMove()
 setInterval(santaMove, 10000)
+
+
+
+// MENU BURGER
+
+const icon_add = document.querySelector('.menu_burger');
+const icon_remove = document.querySelector('.icon_remove');
+const aside_menu = document.querySelector('aside.menu');
+const section_logo = document.querySelector('section.logo');
+
+icon_add.addEventListener('click', () => {
+    aside_menu.classList.add('active')
+    section_logo.classList.add('active')
+    icon_add.classList.add('active')
+
+})
+
+icon_remove.addEventListener('click', () => {
+    aside_menu.classList.remove('active')
+    section_logo.classList.remove('active')
+    icon_add.classList.remove('active')
+})
